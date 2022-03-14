d4c78598c6910876346ccade4027435ee7863322
Genivaldo <cgenivaldo46@gmail.com>
2022-03-14 06:24:25 -0300

    move the player

--- spaceInvaders/index.js
+++ spaceInvaders/index.js
@@ -1,8 +1,8 @@
 const canvas = document.querySelector('canvas');
-const context = canvas.getContext('2d');
+const c = canvas.getContext('2d');
 
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
+canvas.width = innerWidth;
+canvas.height = innerHeight;
 
 class Player {
   constructor() {
@@ -10,6 +10,9 @@ class Player {
       x: 0,
       y: 0,
     };
+
+    this.rotation = 0;
+
     const image = new Image();
     image.src = './img/spaceship.png';
     image.onload = () => {
@@ -25,24 +28,97 @@ class Player {
   }
 
   draw() {
-    if (this.image)
-      context.drawImage(
-        this.image,
-        this.position.x,
-        this.position.y,
-        this.width,
-        this.height
-      );
+
+    c.save()
+    c.translate(
+      player.position.x + player.width / 2,
+      player.position.y + player.height / 2
+    )
+    c.rotate(this.rotation)
+
+    c.translate(
+      -player.position.x - player.width / 2,
+      -player.position.y - player.height / 2
+    )
+    c.drawImage(
+      this.image,
+      this.position.x,
+      this.position.y,
+      this.width,
+      this.height
+    );
+    c.restore();
+  }
+  update() {
+    if (this.image) {
+      this.draw();
+      this.position.x += this.velocity.x;
+    }
   }
 }
 
 const player = new Player();
+const keys = {
+  a: {
+    pressed: false,
+  },
+  d: {
+    pressed: false,
+  },
+  space: {
+    pressed: false,
+  },
+};
 
 function animate() {
   requestAnimationFrame(animate);
-  context.fillStyle = 'black';
-  context.fillRect(0, 0, canvas.width, canvas.height);
-  player.draw();
+  c.fillStyle = 'black';
+  c.fillRect(0, 0, canvas.width, canvas.height);
+  player.update();
+
+  if (keys.a.pressed && player.position.x >= 0) {
+    player.velocity.x = -10;
+    player.rotation = -.15;
+  }
+  else if (keys.d.pressed && player.position.x + player.width <= canvas.width) {
+    player.velocity.x = +10;
+    player.rotation = .15
+  } else {
+    player.velocity.x = 0;
+    player.rotation = 0
+  }
+
 }
 
 animate();
+
+addEventListener('keydown', ({ key }) => {
+  switch (key) {
+    case 'a':
+      console.log('left');
+
+      keys.a.pressed = true;
+      break;
+    case 'd':
+      console.log('right');
+      keys.d.pressed = true;
+      break;
+    case ' ':
+      console.log('space');
+      break;
+  }
+});
+
+addEventListener('keyup', ({ key }) => {
+  switch (key) {
+    case 'a':
+      keys.a.pressed = false;
+      break;
+    case 'd':
+      keys.d.pressed = false;
+      break;
+    case ' ':
+      console.log('space');
+      break;
+  }
+});
